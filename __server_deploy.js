3fa45faa24fb5126bcbbfa3266845dc6b942df15
devnoman01 <devnoman01@gmail.com>
2022-04-30 14:58:00 +0600

    heroku steps added

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/**
+ * ----- one time for pc -----
+ * 1. create heroku
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ *
+ * ----- for each project one time ----
+ * 1. heroku create
+ * 2.
+ *
+ *
+ *
+ */
